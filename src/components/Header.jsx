68a4447bfdc28c630ef2efb6ffb008dd0bd53080
Jona935 <jonarios2511@gmail.com>
2023-02-16 11:08:18 -0600

    fue un cambio chico

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <h1 className=" font-black text-5xl text-center md:w-2/3 mx-auto">
-      Seguimiento de Pacientes{''} <span className=" text-amber-500">Veterinaria</span>
+      Seguimiento de Pacientes{''} Con Github {''} <span className=" text-amber-500">Veterinaria</span>
     </h1>
   )
 }
